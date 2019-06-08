0be277c2acfc0cddd80dd3f846524ffbebf968cc
George Florea Banus <georgefb899@gmail.com>
2019-06-08 19:25:00 +0300

    add license

--- src/view.h
+++ src/view.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 George Florea Banus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef VIEW_H
 #define VIEW_H
 
@@ -23,8 +39,9 @@ signals:
     void requestPage(int number);
 
 public slots:
-    void setImage(QImage image, int number);
-    void imageResized(QImage image, int number);
+    void onImageReady(QImage image, int number);
+    void onImageResized(QImage image, int number);
+    void onScrollBarRangeChanged(int x, int y);
 
 private:
     void resizeEvent(QResizeEvent *e) override;
@@ -35,7 +52,7 @@ private:
     void addRequest(int number);
     void delRequest(int number);
     bool hasRequest(int number) const;
-    bool isInView(int imgTop, int imgBot);
+    bool isInView  (int imgTop, int imgBot);
 
     QGraphicsScene  *m_scene;
     QString          m_manga;
@@ -44,6 +61,8 @@ private:
     QVector<int>     m_start;
     QVector<int>     m_end;
     QVector<int>     m_requestedPages;
+    int              m_firstVisible = -1;
+    double           m_firstVisibleOffset = 0.0f;
 };
 
 #endif // VIEW_H
