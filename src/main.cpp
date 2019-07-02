39a063163976c8103678f580f798f455deef1a88
George Florea Banus <georgefb899@gmail.com>
2019-07-02 03:12:52 +0300

    change license to gpl v3

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 /*
- * Copyright 2019 George Florea Banus
+ * Copyright 2019 Florea Banus George <georgefb899@gmail.com>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "mainwindow.h"
@@ -26,25 +27,14 @@ int main(int argc, char *argv[])
 
     KLocalizedString::setApplicationDomain("mangareader");
     KAboutData aboutData(
-        // The program name used internally. (componentName)
         QStringLiteral("mangareader"),
-        // A displayable program name string. (displayName)
         i18n("Manga Reader"),
-        // The program version string. (version)
         QStringLiteral("1.0.0"),
-        // Short description of what the app does. (shortDescription)
         i18n("Manga reader for local files."),
-        // The license this code is released under
         KAboutLicense::GPL_V3,
-        // Copyright Statement (copyrightStatement = QString())
         i18n("(c) 2019"),
-        // Optional text shown in the About box.
-        // Can contain any information desired. (otherText)
         QStringLiteral(),
-        // The program homepage string. (homePageAddress = QString())
         QStringLiteral("https://gitlab.com/g-fb/manga-reader"),
-        // The bug report email address
-        // (bugsEmailAddress = QLatin1String("submit@bugs.kde.org")
         QStringLiteral("https://gitlab.com/g-fb/manga-reader/issues"));
 
     aboutData.addAuthor(
