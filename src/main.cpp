bdbbce7b90d3e0c2c94b0f0c07c8f5edd73daf7d
George Florea Banus <georgefb899@gmail.com>
2019-08-02 22:50:28 +0300

    change version to 1.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     KAboutData aboutData(
         QStringLiteral("mangareader"),
         i18n("Manga Reader"),
-        QStringLiteral("1.0.0"),
+        QStringLiteral("1.0.1"),
         i18n("Manga reader for local files."),
         KAboutLicense::GPL_V3,
         i18n("(c) 2019"),
