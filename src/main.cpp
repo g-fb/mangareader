3bfd5fce86411ca032c0c5cbee9c16ad5ab153fb
George Florea Bănuș <georgefb899@gmail.com>
2020-03-17 19:40:40 +0200

    - bump version to 1.0.5

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     KAboutData aboutData(
         QStringLiteral("mangareader"),
         i18n("Manga Reader"),
-        QStringLiteral("1.0.4"),
+        QStringLiteral("1.0.5"),
         i18n("Manga reader for local files."),
         KAboutLicense::GPL_V3,
         i18n("(c) 2019"),
