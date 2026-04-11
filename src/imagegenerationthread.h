daf19170cfe25073f1789503cb269d96dca584e5
George Florea Bănuș <georgefb899@gmail.com>
2026-04-11 03:37:11 +0300

    fix licenses
    change cmake files license to BSD-2-Clause
    add license to files without

--- src/imagegenerationthread.h
+++ src/imagegenerationthread.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2019 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef IMAGEGENERATIONTHREAD_H
 #define IMAGEGENERATIONTHREAD_H
 
--- src/imagerequest.h
+++ src/imagerequest.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2019 George Florea Bănuș <georgefb899@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef IMAGEREQUEST_H
 #define IMAGEREQUEST_H
 
